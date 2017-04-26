59316c18ad80b21cf68d44cf007eaa6fda74b04f
VIN <vincent657.leclerc@gmail.com>
2017-04-26 13:48:42 -0400

    Main Menu + Level 1 and 2 + Flags color set

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadOnClick (string scene){
+		SceneManager.LoadScene (scene);
+	}
+
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+
+	}
+}
