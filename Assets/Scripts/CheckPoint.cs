a6410c234bd9abbec0627d17d5f32675c722b13c
VIN <vincent657.leclerc@gmail.com>
2017-04-26 14:42:14 -0400

    CheckPoint Louis Update

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 [System.Serializable]
 public class CheckPointData {
+	int id;
+	public CheckPointData(CheckPoint checkpoint){
+		this.id = checkpoint.id;
+	}
 
+	public CheckPoint GetCheckPoint() {
+		return CheckPoint.mappedCP [id];
+	}
 }
 
 public class CheckPoint : MonoBehaviour
 {
 	public bool isInit;
 	public CheckPointData data;
+	public static Dictionary<int, CheckPoint> mappedCP;
+	private static IDManager idManager;
+	internal int id;
 
-	public void Start(){
+
+	public void Awake(){
+		if (idManager == null)
+			idManager = new IDManager();
+		if (mappedCP == null)
+			mappedCP = new Dictionary<int, CheckPoint> ();
+		this.id = idManager.GetNewID ();
 		if (isInit) {
 			MapSaveState.current.serializable.initCheck = data;
 		}
 	}
 
 	public void OnTriggerEnter(Collider other){
-		if (other.tag == "player") {
+		if (other.tag == "Player") {
 			MapSaveState.current.serializable.finalCheck = data;
 		}
 	}
