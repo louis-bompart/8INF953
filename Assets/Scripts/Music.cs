1e0aadba599df289fc70dc1c1c1e3a04d8a0b1fe
VIN <vincent657.leclerc@gmail.com>
2017-05-02 16:13:10 -0400

    mini bug

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	public AudioClip warpSound;
+	public AudioSource noise;
+	// Use this for initialization
+	void Awake () {
+		noise.PlayOneShot (warpSound);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
